1626b54bbc48a003106a48e3b65742069b6f5a50
Ken Zalewski <zalewski.senate@gmail.com>
2023-10-24 01:58:41 -0400

    Add ability to read CSV from stdin; remove CR (carriage return, aka ^M, aka \r) from end of line

--- csv2xsv.sh
+++ csv2xsv.sh
@@ -6,17 +6,24 @@
 # Author: Ken Zalewski
 # Organization: New York State Senate
 # Date: 2010-05-06
+# Revised: 2023-10-24 - Add ability to read CSV from stdin; remove CR (^M)
 #
 prog=`basename $0`
 script_dir=`dirname $0`
 parser="$script_dir/parse_xsv.awk"
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $prog csv_file" >&2
+if [ $# -gt 1 ]; then
+  echo "Usage: $prog [csv_file]" >&2
   exit 1
+elif [ $# -eq 0 ]; then
+  cfile="-"
+else
+  cfile="$1"
+  if [ ! -r "$cfile" ]; then
+    echo "$prog: $cfile: CSV file not found" >&2
+    exit 2
+  fi
 fi
-
-cfile="$1"
 
 if [ "$prog" = "csv2ssv.sh" ]; then
   newline="|"
@@ -29,7 +36,9 @@ else
   delim="|"
 fi
 
-cat "$cfile" | $script_dir/convert_nonprintables.sh | \
+cat "$cfile" | \
+sed 's;$;;' | \
+$script_dir/convert_nonprintables.sh | \
 awk --assign newline="$newline" --assign delim="$delim" --file "$parser" --source '
 BEGIN {
 }
